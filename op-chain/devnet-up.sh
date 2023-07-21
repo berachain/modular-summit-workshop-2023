fee2e49d0aea5cc073f60204fe14ce62fd208624
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-07-22 00:09:35 +0200

    add dockercompose

--- op-chain/devnet-up.sh
+++ op-chain/devnet-up.sh
@@ -82,8 +82,8 @@ fi
 (
   cd ops-bedrock
   echo "Bringing up L1..."
-  DOCKER_BUILDKIT=1 docker-compose -f docker-compose-devnet.yml build --progress plain
-  docker-compose -f docker-compose-devnet.yml up -d l1
+  DOCKER_BUILDKIT=1 docker-compose build --progress plain
+  docker-compose up -d l1
   wait_up $L1_URL
 )
 
@@ -91,7 +91,7 @@ fi
 (
   cd ops-bedrock
   echo "Bringing up L2..."
-  docker-compose -f docker-compose-devnet.yml up -d l2
+  docker-compose up -d l2
   wait_up $L2_URL
 )
 
@@ -101,14 +101,14 @@ L2OO_ADDRESS="0x6900000000000000000000000000000000000000"
 (
   cd ops-bedrock
   echo "Bringing up devnet..."
-  docker-compose -f docker-compose-devnet.yml up -d da
+  docker-compose up -d da
   wait_up http://localhost:26659/header/1
   export CELESTIA_NODE_AUTH_TOKEN="$(docker exec ops-bedrock-da-1 celestia bridge auth admin --node.store /bridge)"
   L2OO_ADDRESS="$L2OO_ADDRESS" \
-      docker-compose -f docker-compose-devnet.yml up -d op-proposer op-batcher
+      docker-compose up -d op-proposer op-batcher
 
   echo "Bringing up stateviz webserver..."
-  docker-compose -f docker-compose-devnet.yml up -d stateviz
+  docker-compose up -d stateviz
 )
 
 echo "Devnet ready."
